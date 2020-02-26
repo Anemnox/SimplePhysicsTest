9b462eb3c1997c8132d48af3841b0e6bff912225
Andrew <anemnox@gmail.com>
2020-02-25 18:34:36 -0800

    removed app init

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> pooopooo peepeeeee </p>
+      <p> Simon was here </p>
+      <p> Daelyn is Cool </p>
+      <p> Peyton says Honk </p>
     </div>
   );
 }
---
+++ client/src/components/GraphicsCanvas.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class GraphicsCanvas extends React.Component {
+
+
+
+}
+
+export default GraphicsCanvas;
