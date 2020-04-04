23766c397664f82910917f74d16bb74afc5c890d
Andrew <anemnox@gmail.com>
2020-04-04 16:27:27 -0700

    update collision detection

--- client/src/components/graphics/GilbertCore.js
+++ client/src/components/graphics/GilbertCore.js
@@ -3,16 +3,28 @@ import * as THREE from "three";
 let Vector3 = THREE.Vector3;
 let Euler = THREE.Euler;
 
-Euler.prototype.add = function(euler) {
+Euler.prototype.add = function (euler) {
    this.x += euler.x;
    this.y += euler.y;
    this.z += euler.z;
+   return this;
 }
 
 Euler.prototype.scale = function (scalar) {
    this.x *= scalar;
    this.y *= scalar;
    this.z *= scalar;
+   return this;
+}
+
+Euler.prototype.lengthSq = function () {
+   return this.x * this.x +
+         this.y * this.y +
+         this.z * this.z;
+}
+
+Vector3.prototype.toString = function () {
+   return "{" + this.x + ", " + this.y + ", " + this.z + "}";
 }
 
 class GameScene {
@@ -36,7 +48,10 @@ class GameScene {
                obj1.applyForce(force);
             });
 
-            obj1.updateFuture(ms);
+            obj1.updateVelocity(ms);
+
+            let velVector = obj1.velocity.clone();
+            let rotVector = obj1.rotationalVelocity.clone();
 
             this.objects.forEach((obj2, i2) => {
                if(i1 !== i2) {
@@ -44,27 +59,186 @@ class GameScene {
                   let sphere2 = new THREE.Sphere();
                   sphere1.copy(obj1.geoBound.boundingSphere);
                   sphere2.copy(obj2.geoBound.boundingSphere);
-                  sphere1.center = obj1.position;
+                  sphere1.center = obj1.position.clone().add(velVector);
                   sphere2.center = obj2.position;
                   if(sphere1.intersectsSphere(sphere2)) {
                      obj1.graphicsObject.material.color.setHex( 0xff0000 );
+
+                     let temp = this.collisionCheck(velVector, rotVector, obj1, obj2);
+                     if(temp[0].lengthSq() < velVector.lengthSq()) {
+                        velVector = temp[0];
+                     }
+                     if(temp[1].lengthSq() < rotVector.lengthSq()) {
+                        rotVector = temp[1];
+                     }
                   } else {
                      obj1.graphicsObject.material.color.setHex( 0x333333 );
                   }
                }
             });
-            obj1.acceptFuture();
+
+            obj1.rotate(rotVector);
+            obj1.move(velVector);
          }
       });
    }
 
-   // Add/Remove objects from the Game Scene
-   // @param {GameObject} reference to the object you would like to remove.
-   // @param {array} array to add/remove from.
+   // Check the movement constraints of a moving obj1 on a stationary obj2 and
+   // returns a velocity
+   //
+   collisionCheck(vel, objRot, obj1, obj2) {
+      let collisionTime = 2;
+      let normalForce = new Vector3(0, 0, 0);
+      let contactPoint = [];
+      let geo1 = obj1.geoBound;
+      let geo2 = obj2.geoBound;
+      let constrainedVelocity = vel.clone();
+      let constrainedRotation = objRot.clone();
+
+      geo1.vertices.forEach((vertex, i) => {
+         let velocity = vel.clone();
+         let point = vertex.clone().applyEuler(obj1.rotation);
+
+         let isRotating = obj1.rotationalVelocity.lengthSq() !== 0;
+
+         if(isRotating) {
+            velocity =
+               point.clone().sub(vertex.clone().applyEuler(
+                     obj1.rotation.clone().add(objRot))).add(vel);
+         }
+
+         point.add(obj1.position);
+
+         geo2.faces.forEach((face, i) => {
+            let faceNormal = face.normal.clone().applyEuler(obj2.rotation);
+            if((faceNormal.dot(velocity) <= 0)) {
+               let facePoint = geo2.vertices[face.a].clone()
+                              .applyEuler(obj2.rotation).add(obj2.position);
+               let time =
+                  timeOfIntersectPlaneLine(
+                     velocity, point,
+                     faceNormal, facePoint
+                  );
+               //debugger;
+               if(time >= -0.01 && time < 1.1) {
+                  let a = geo2.vertices[face.a].clone().applyEuler(obj2.rotation);
+                  let b = geo2.vertices[face.b].clone().applyEuler(obj2.rotation);
+                  let c = geo2.vertices[face.c].clone().applyEuler(obj2.rotation);
+                  let relPointVect = new Vector3(
+                        velocity.x * time + point.x,
+                        velocity.y * time + point.y,
+                        velocity.z * time + point.z
+                     );
+                  let p = relPointVect.clone().sub(obj2.position);
+                  if(isInsideTriangle(p, a, b, c)) {
+                     if(time > 1)
+                        time = 1;
+
+                     if(time < collisionTime) {
+                        //debugger;
+                        normalForce = faceNormal.multiplyScalar(-1);
+                        contactPoint = relPointVect;
+                        constrainedVelocity =
+                           velocity.clone().multiplyScalar(time);;
+                     }
+                  }
+               }
+            }
+         });
+      });
+
+         ///**
+      geo2.vertices.forEach((vertex, i) => {
+         let velocity = vel.clone().multiplyScalar(-1);
+         geo1.faces.forEach((face, i) => {
+            let point = vertex.clone().applyEuler(obj2.rotation).add(obj2.position);
+            let faceNormal = face.normal.clone().applyEuler(obj1.rotation);
+
+            if((faceNormal.dot(velocity) <= 0)) {
+               let facePoint = geo1.vertices[face.a].clone().applyEuler(obj1.rotation).add(obj1.position);
+               let time =
+                  timeOfIntersectPlaneLine(
+                     velocity, point,
+                     faceNormal, facePoint
+                  );
+
+               if(time >= 0 && time <= 1.01) {
+                  let a = geo1.vertices[face.a].clone().applyEuler(obj1.rotation);
+                  let b = geo1.vertices[face.b].clone().applyEuler(obj1.rotation);
+                  let c = geo1.vertices[face.c].clone().applyEuler(obj1.rotation);
+                  let relPointVect = new Vector3(
+                     velocity.x * time + point.x,
+                     velocity.y * time + point.y,
+                     velocity.z * time + point.z
+                  );
+                  let p = relPointVect.clone().sub(obj1.position);
+
+                  //debugger;
+                  if(isInsideTriangle(p, a, b, c)) {
+                     if(time > 1)
+                        time = 1;
+                     time = (1 - time);
+
+                     if(time < collisionTime) {
+                        //debugger;
+                        normalForce = faceNormal.multiplyScalar(-1);
+                        contactPoint = relPointVect;
+                        constrainedVelocity =
+                           velocity.clone().multiplyScalar(-1 * time);;
+                     }
+                  }
+               }
+            }
+         });
+      });
+
+      // */
+
+      if(contactPoint !== null) {
+         let vai = Math.cos(Math.PI - vel.angleTo(normalForce)) *
+                     vel.length() * 1000;
+         let vbi = Math.cos(Math.PI - obj2.velocity.angleTo(normalForce)) *
+                     obj2.velocity.length() * 1000;
+
+         let vaf = (obj1.mass - obj2.mass) / (obj1.mass + obj2.mass) * vai +
+                     (2 * obj2.mass) / (obj1.mass + obj2.mass) * vbi;
+         let vbf = (2 * obj1.mass) / (obj1.mass + obj2.mass) * vai +
+                     (obj2.mass - obj1.mass) / (obj1.mass + obj2.mass) * vbi;
+
+         let nf = normalForce.normalize()
+         obj1.applyForce(function () {
+            return new ForceVector(
+               nf.clone().multiplyScalar((vai - vaf) * obj1.mass),
+               contactPoint.clone().sub(obj1.position)
+            )
+         });
+         obj2.applyForce(function () {
+            return new ForceVector(
+               nf.clone().multiplyScalar((vbi - vbf) * obj2.mass),
+               contactPoint.clone().sub(obj2.position)
+            )
+         });
+      }
+
+      return [ constrainedVelocity, constrainedRotation ];
+   }
+
+   /**
+      Add objects from the Game Scene
+
+      @param {GameObject} object reference to the object you would like to add.
+   */
    add(object) {
       this.objects.push(object);
       this.scene.add(object.graphicsObject);
    }
+
+   /**
+      Remove objects from the Game Scene
+
+      @param {GameObject} object reference to the object you would like to
+               remove.
+   */
    remove(object) {
       let index = this.objects.indexOf(object);
       if(index !== -1) {
@@ -73,6 +247,12 @@ class GameScene {
       }
    }
 
+   /**
+      Adds a force vector field that acts on objects in the Game Scene
+
+      @param {Function} func a function that should return a ForceVector object
+               based on a given x, y, z coordinate.
+   */
    addVectorField(func) {
       if(func instanceof Function)
          this.forceVectorField.push(func);
@@ -97,11 +277,11 @@ class GameObject {
       this.geoBound.computeBoundingSphere();
    }
 
-   updateFuture(ms) {
+   updateVelocity(ms) {
       this.forces.forEach((force) => {
-         this.velocity.x += force.force.x / this.mass;
-         this.velocity.y += force.force.y / this.mass;
-         this.velocity.z += force.force.z / this.mass;
+         this.velocity.x += force.force.x / this.mass * ms;
+         this.velocity.y += force.force.y / this.mass * ms;
+         this.velocity.z += force.force.z / this.mass * ms;
       });
       this.forces = [];
 
@@ -109,8 +289,6 @@ class GameObject {
       let drv = this.rotationalVelocity.clone();
       v.multiplyScalar(ms);
       drv.scale(ms);
-      this.position.add(v);
-      this.rotation.add(drv);
    }
 
    acceptFuture() {
@@ -130,6 +308,16 @@ class GameObject {
       }
    }
 
+   move(vector) {
+      this.position.add(vector);
+      this.acceptFuture();
+   }
+
+   rotate(euler) {
+      this.rotation.add(euler);
+      this.acceptFuture();
+   }
+
    setPosition(x, y, z) {
       this.position.set(x, y, z);
       this.acceptFuture();
@@ -139,21 +327,6 @@ class GameObject {
       this.rotation.set(x, y, z);
       this.acceptFuture();
    }
-
-   //
-   //
-   //
-   updateCollision(geometry) {
-      let obj1 = this.geoBound.clone();
-      let obj2 = geometry.clone();
-      this.geoBound.forEach((vertice, i) => {
-         let vert = vertice.clone();
-         vert.applyEuler(this.rotation);
-         vert.add(this.position);
-
-
-      });
-   }
 }
 
 class ForceVector {
@@ -161,6 +334,66 @@ class ForceVector {
       this.force = force;
       this.position = position;
    }
+}
+
+/**
+   Returns the time of intesection between a line and plane.
+   One unit of time is represented by the given vector.
+
+   @param {Vector3} lineVector a vector the represents the path of a point
+   @param {Vector3} linePoint a vector that represents the point at t = 0
+            for the path of the point
+   @param {Vector3} planeNormal a vector that represents the normal vector
+            of the plane
+   @param {Vector3} planePoint a vector that represents a point on the plane.
+*/
+function timeOfIntersectPlaneLine(lineVector, linePoint,
+                                 planeNormal, planePoint) {
+   return ((-planeNormal.x * ( linePoint.x - planePoint.x ) -
+               planeNormal.y * ( linePoint.y - planePoint.y ) -
+               planeNormal.z * ( linePoint.z - planePoint.z )) /
+               ((planeNormal.x * lineVector.x) +
+               (planeNormal.y * lineVector.y) +
+               (planeNormal.z * lineVector.z)));
+}
+
+/**
+   Returns true or false based on if the given point is within the bounds
+   of a triangle with the given coordinates. Assumes that the point is on
+   the same plane as the triangle.
+
+   @param {Vector3} point a point in 3d space
+   @param {Vector3} a first vertex of the triangle
+   @param {Vector3} b second vertex of the triangle
+   @param {Vector3} c third vertex of the triangle
+*/
+function isInsideTriangle(point, a, b, c) {
+   let v1 = b.clone().sub(a);
+   let v2 = c.clone().sub(a);
+   let v3 = point.clone().sub(a);
+
+   let omega1 = v1.angleTo(v2);
+   let omega2 = v2.angleTo(v3);
+   let omega3 = v1.angleTo(v3);
+   let tot = omega3 + omega2 - omega1;
+
+   // check angle is between
+   if(tot < 0.001 && tot > -0.001) {
+      v1 = a.sub(b);
+      v2 = c.sub(b);
+      v3 = point.clone().sub(b);
+
+      omega1 = v1.angleTo(v2);
+      omega2 = v2.angleTo(v3);
+      omega3 = v1.angleTo(v3);
+      tot = omega3 + omega2 - omega1;
+      //debugger;
+
+      if(tot < 0.001 && tot > -0.001) {
+         return true
+      }
+   }
+   return false;
 }
 
 export { GameScene, GameObject, ForceVector, Vector3 };
--- client/src/components/graphics/VisualCore.js
+++ client/src/components/graphics/VisualCore.js
@@ -9,40 +9,81 @@ gameScene.cameras.push(
    )
 );
 
-gameScene.cameras[0].position.set(0, 0, 5);
-gameScene.cameras[0].rotation.set(-0.0, 0, 0);
+gameScene.cameras[0].position.set(0, 2, 7);
+gameScene.cameras[0].rotation.set(-0, 0, 0);
 //gameScene.renderer.antialias = true;
 gameScene.scene.background = new THREE.Color( 0xDDDDDD );
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
+var geometry2 = new THREE.SphereGeometry(1, 20, 20);
+
 var material = new THREE.MeshBasicMaterial( { color: 0x4477aa } );
 material.wireframe = true;
 var material2 = new THREE.MeshBasicMaterial( { color: 0x222222 } );
 
-var cube = new THREE.Mesh( geometry, material );
-var plane = new THREE.BoxGeometry(5, 0.1, 5);
+var cube = new THREE.Mesh( geometry2, material );
+var plane = new THREE.BoxGeometry(30, 0.1, 5);
+var walls = new THREE.BoxGeometry(0.5, 5, 5);
 var floor = new THREE.Mesh( plane, material2 );
-cube.position.set(0, 2  , 0);
+var floor2 = new THREE.Mesh( plane, material2 );
+var walls1 = new THREE.Mesh( walls, material2 );
+var walls2 = new THREE.Mesh( walls, material2 );
+var walls3 = new THREE.Mesh( walls, material2 );
+var walls4 = new THREE.Mesh( walls, material2 );
+
+floor.rotation.z = -0;
+floor2.rotation.z = -0.01;
+floor.position.x = 0;
+floor2.position.x = 0;
+walls1.position.x = -3;
+walls2.position.x = 3;
+walls1.position.y = 3;
+walls2.position.y = 3;
+
+
+cube.position.set(-3, 5, 0);
+let cube2 = new THREE.Mesh( geometry, material );
+cube2.position.set(0, 5, 0);
 let object = new GilbertCore.GameObject(cube);
+let object2 = new GilbertCore.GameObject(cube2);
+
 let floors = new GilbertCore.GameObject(floor);
+let floors2 = new GilbertCore.GameObject(floor2);
+let wall1 = new GilbertCore.GameObject(walls1);
+let wall2 = new GilbertCore.GameObject(walls2);
+let wall3 = new GilbertCore.GameObject(walls3);
+let wall4 = new GilbertCore.GameObject(walls4);
 
-cube.rotation.z = 1;
+cube.rotation.z = 0.1;
 floors.needsUpdate = false;
-
-object.velocity.y = -0.00;
-object.rotationalVelocity.z = 0;
-console.log(cube.geometry);
+floors.mass = 20;
+floors2.needsUpdate = false;
+wall1.needsUpdate = false;
+wall2.needsUpdate = false;
+wall3.needsUpdate = false;
+wall4.needsUpdate = false;
+wall2.mass = 10000000000000000;
+object.velocity.x = 0.01;
+object.mass = 2;
+object2.rotationalVelocity.z = 0.0;
+//object.needsUpdate = false;
+//console.log(cube.geometry);
 gameScene.add(object);
+gameScene.add(object2);
 gameScene.add(floors);
+gameScene.add(wall2);
+
+//gameScene.add(floors2);
 
 
+//**
 gameScene.addVectorField((pos, mass) => {
    return new GilbertCore.ForceVector(
-      new GilbertCore.Vector3(0, -0.1 * mass, 0),
+      new GilbertCore.Vector3(0, -0.8 * mass, 0),
       new GilbertCore.Vector3(0, 0, 0)
    )
 });
-
+//*/
 
 setInterval(() => {
    gameScene.update(1);
